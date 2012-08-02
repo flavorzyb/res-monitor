49392b5f5a2cbf54e496e8ea4673962d26145cb9
flavorzyb2008 <flavorzyb2008@gmail.com>
2012-08-02 16:34:09 +0800

    add windows run bat

---
+++ startResourceMonitorServer.bat
@@ -0,0 +1,4 @@
+@D:
+@cd \
+@cd resource_monitor
+@java -cp libs/jnotify-0.94.jar;libs/svnkit-1.7.4-v1.jar;libs/antlr-runtime-3.4.jar;libs/jna-3.4.0.jar;libs/sequence-library-1.0.2.jar;libs/sqljet-1.1.1.jar;libs/svnkit-cli-1.7.4-v1.jar;libs/svnkit-cli-1.7.4-v1.jar;libs/svnkit-javahl16-1.7.4-v1.jar;libs/trilead-ssh2-1.0.0-build215.jar;resource-monitor-1.0-SNAPSHOT.jar -Djava.library.path=libs -Duser.config=configs/config.xml com.zhuyanbin.app.App
